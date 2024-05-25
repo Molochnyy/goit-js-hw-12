f104c64d7186fe7ea8a9880113f9a2c0aed0ea4e
Molochnyy <153619807+Molochnyy@users.noreply.github.com>
2024-05-25 16:01:43 +0300

    ff

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import iziToast from 'izitoast';
+
+const API_KEY = '43838744-76530a55bebff011fa4d493be';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export const fetchImages = async (query, page = 1, perPage = 15) => {
+    try {
+        const response = await axios.get(BASE_URL, {
+            params: {
+                key: API_KEY,
+                q: query,
+                image_type: 'photo',
+                per_page: perPage,
+                page: page,
+            },
+        });
+        return response.data;
+    } catch (error) {
+        iziToast.error({
+            title: 'Error',
+            message: 'Failed to fetch images. Please try again later.',
+        });
+        throw error;
+    }
+};
